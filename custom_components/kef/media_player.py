3fd6f4db4e0d512bf8657df8cb90e0c6caf899ec
Bas Nijholt <basnijholt@gmail.com>
2020-01-02 22:36:20 +0100

    use getmac

--- custom_components/kef/media_player.py
+++ custom_components/kef/media_player.py
@@ -1,9 +1,12 @@
 """Platform for the KEF Wireless Speakers."""
 
 from datetime import timedelta
+from functools import partial
+import ipaddress
 import logging
 
 from aiokef.aiokef import AsyncKefSpeaker
+from getmac import get_mac_address
 import voluptuous as vol
 
 from homeassistant.components.media_player import (
@@ -18,8 +21,6 @@ from homeassistant.components.media_player import (
 )
 from homeassistant.const import (
     CONF_HOST,
-    CONF_LATITUDE,
-    CONF_LONGITUDE,
     CONF_NAME,
     CONF_PORT,
     CONF_TYPE,
@@ -81,11 +82,11 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
     host = config[CONF_HOST]
     speaker_type = config[CONF_TYPE]
-    port = config.get(CONF_PORT)
-    name = config.get(CONF_NAME)
-    maximum_volume = config.get(CONF_MAX_VOLUME)
-    volume_step = config.get(CONF_VOLUME_STEP)
-    inverse_speaker_mode = config.get(CONF_INVERSE_SPEAKER_MODE)
+    port = config[CONF_PORT]
+    name = config[CONF_NAME]
+    maximum_volume = config[CONF_MAX_VOLUME]
+    volume_step = config[CONF_VOLUME_STEP]
+    inverse_speaker_mode = config[CONF_INVERSE_SPEAKER_MODE]
     standby_time = config.get(CONF_STANDBY_TIME)
 
     sources = SOURCES[speaker_type]
@@ -99,9 +100,16 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         sources,
     )
 
-    latitude = config.data[CONF_LATITUDE]
-    longitude = config.data[CONF_LONGITUDE]
-    unique_id = f"kef-{latitude}-{longitude}"
+    try:
+        if ipaddress.ip_address(host).version == 6:
+            mode = "ip6"
+        else:
+            mode = "ip"
+    except ValueError:
+        mode = "hostname"
+    mac = await hass.async_add_executor_job(partial(get_mac_address, **{mode: host}))
+
+    unique_id = f"kef-{mac}"
 
     media_player = KefMediaPlayer(
         name,
